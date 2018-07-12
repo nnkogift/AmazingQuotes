33d2d4df12ad2f196956cf8e26e6458d04a6cf61
nnkogift <nnkogift@gmail.com>
2018-07-12 23:15:57 +0300

    Basic settings of the website

---
+++ amazingQuotes/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AmazingquotesConfig(AppConfig):
+    name = 'amazingQuotes'
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/gian/PycharmProjects/AmazingQuotes/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
