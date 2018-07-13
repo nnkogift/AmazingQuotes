220816b3b2ef3ff9948737302e65540b5dd30a8c
nnkogift <nnkogift@gmail.com>
2018-07-13 10:37:02 +0300

    Adding the settings for the database and security key

--- AmazingQuotes/settings.py
+++ AmazingQuotes/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import dj_database_url
+from decouple import config
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -20,7 +23,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3l!d2_lez$#_y4kv597_p_&a^!xhbtu3uuyab(!)6l@tgzu6e+'
+
+#SECRET_KEY = '3l!d2_lez$#_y4kv597_p_&a^!xhbtu3uuyab(!)6l@tgzu6e+'
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
