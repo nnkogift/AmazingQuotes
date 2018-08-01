50bd380dd492a3ee1fc5a23e0c0bfc807cad7449
nnkogift <nnkogift@gmail.com>
2018-08-01 16:42:51 +0300

    The application

--- amazingQuote/admin.py
+++ amazingQuote/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Event, Product, AmazingQuotesAbout, TeamMember, Value, Order,Quote, TrainingTopic
+from .models import Event, Product, AmazingQuotesAbout, TeamMember, Value, Order, Quote, TrainingTopic
 
 
 # Register your models here.
