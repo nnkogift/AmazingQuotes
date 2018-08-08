236d670dacbb69ac45c2d321f503b49a71ab8e26
Gift Nnko <nnkogift@hotmail.com>
2018-08-08 11:26:23 +0300

    Update settings.py

--- AmazingQuotes/settings.py
+++ AmazingQuotes/settings.py
@@ -90,19 +90,12 @@ WSGI_APPLICATION = 'AmazingQuotes.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else:
-    DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+
+DATABASES = {
+'default': dj_database_url.config(
+    default=config('DATABASE_URL')
     )
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
