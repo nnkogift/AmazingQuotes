b5d8da62fb32562f19465b38103820c5edbce5fd
Gift Nnko <nnkogift@hotmail.com>
2018-08-08 11:25:18 +0300

    Update settings.py

--- AmazingQuotes/settings.py
+++ AmazingQuotes/settings.py
@@ -99,12 +99,10 @@ if DEBUG:
     }
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
     }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
