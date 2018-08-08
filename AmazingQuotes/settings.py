21a6de417736e281cd1319173cb81dbd709ef411
Gift Nnko <nnkogift@hotmail.com>
2018-08-08 11:20:27 +0300

    Update settings.py

--- AmazingQuotes/settings.py
+++ AmazingQuotes/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = decouple.config('DEBUG', default=True, cast=bool)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
