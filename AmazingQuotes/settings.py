fd8e0ec5583ea30dd4644967b9914cd36bc1e41e
Gian <nnkogift@hotmail.com>
2018-08-12 20:56:31 +0300

    Integrated the google api storage. now working final migrations in place

--- AmazingQuotes/settings.py
+++ AmazingQuotes/settings.py
@@ -92,18 +92,18 @@ WSGI_APPLICATION = 'AmazingQuotes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-'default': config(
-    default=config('DATABASE_URL')
-    )
-}
-#
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
+# 'default': config(
+#     default=config('DATABASE_URL')
+#     )
 # }
+#
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
